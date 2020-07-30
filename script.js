c5e03a9aab203cd8ad3c85b39090dea07d6d0653
Ian Pyeatt <pyeatt08@gmail.com>
2020-07-30 18:36:43 -0500

    set up initial docs/formatted HTML with rows/cols

---
+++ script.js
@@ -0,0 +1,24 @@
+$("#currentDay").text(moment().format("dddd MMMM Do YYYY"));
+
+let today = new Date();
+console.log(today);
+timeNow = today.getHours();
+console.log(timeNow);
+// changing text area colors based on time of day
+// function changeHours() {}
+
+
+// let currentTime = 4;
+// if (currentTime < timeNow) {
+//     $(".colorArea").addClass("past");
+// } else if () {
+    
+//     $(".colorArea").addClass("past");
+// } else () {
+    
+//     $(".colorArea").addClass("past");
+// }
+
+// $(".saveBtn").on("click", () => {
+//   console.log(4);
+// });
